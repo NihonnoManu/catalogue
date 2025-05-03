49643a7ba2e03b51ee3de041a4c0693870ba975e
NihonnoManu <nihonnomanu@gmail.com>
2025-05-03 02:17:11 +0200

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,8 @@
+import dotenv from 'dotenv';
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
-import dotenv from 'dotenv';
+
 
 // Load environment variables from .env file
 dotenv.config();
